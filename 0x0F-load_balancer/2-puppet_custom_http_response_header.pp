ee2178244f20bf1d1150480f5ef7225f4eb33a8f
yoniem <yonimphini@gmail.com>
2024-02-03 10:44:20 -0800

    update

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,56 @@
+#!/usr/bin/env bash
+## Installs Nginx with puppet with the following configurations:
+#+    Listens on port 80.
+#+    Returns a page containing "Hello World!" when queried
+#+     at the root with a curl GET request.
+#+    Configures /redirect_me as a "301 Moved Permanently".
+#+    Includes a custom 404 page containing "Ceci n'est pas une page".
+#+    Contains a custom HTTP header named X-Served-By.
+#+    The value of the HTTP header is the hostname of the running server.
+
+package { 'nginx':
+  ensure => installed,
+}
+
+file { '/var/www/html':
+  ensure => directory,
+}
+
+file { '/var/www/html/index.html':
+  ensure  => present,
+  content => 'Hello World!',
+}
+
+file { '/var/www/html/404.html':
+  ensure  => present,
+  content => "Ceci n'est pas une page",
+}
+
+file { '/etc/nginx/sites-available/default':
+  ensure  => present,
+  content => '
+    server {
+      listen 80 default_server;
+      listen [::]:80 default_server;
+      add_header X-Served-By $hostname;
+      root /var/www/html;
+      index index.html index.htm;
+
+      location /redirect_me {
+        return 301 http://youtube.com/;
+      }
+
+      error_page 404 /404.html;
+      location /404 {
+        root /var/www/html;
+        internal;
+      }
+    }
+  ',
+}
+
+service { 'nginx':
+  ensure  => running,
+  enable  => true,
+  require => File['/etc/nginx/sites-available/default'],
+}
